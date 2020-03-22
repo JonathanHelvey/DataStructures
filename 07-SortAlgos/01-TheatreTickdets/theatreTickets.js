6de35ee6a7daf28e13e61361f2e5980a0656958a
JonathanHelvey <jonathanhelvey27@gmail.com>
2020-03-21 22:02:20 -0400

    theatre tickets

---
+++ 07-SortAlgos/01-TheatreTickdets/theatreTickets.js
@@ -0,0 +1,39 @@
+
+
+// const waysToUseTickets = (array) => {
+//   let combos = []
+//   if(array.length < 2) {
+//     combos.length = 0
+//   }
+
+//   for(let i = 0; i <= array.length; i++) {
+//     for(let j = i + 1; j <= array.length - 1; j++) {
+//       for(let k = j + 1; k <= array.length - 1; k++) {
+//         combos.push([array[i], array[j], array[k]])
+//       }
+//     }
+//   }
+//   combos = Array.from(new Set(combos.map(JSON.stringify)), JSON.parse)
+//   return combos.length
+// }
+
+const waysToUseTickets = (array) => {
+  array.sort((a, b) => a - b)
+  let combos = []
+  let left;
+  let right;
+
+  for (let i = 0; i <= array.length - 1; i++) {
+    left = i + 1
+    right = array.length - 1
+    while(left < right) {
+      left++
+      right--
+      combos.push([array[i], array[left], array[right]])
+    }
+  }
+  combos = Array.from(new Set(combos.map(JSON.stringify)), JSON.parse)
+  return combos.length
+}
+
+console.log(waysToUseTickets([2, 2, 2, 2]))
