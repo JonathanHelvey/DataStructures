2a80b19823a325236a1dde5f395b9d397ff150cb
JonathanHelvey <jonathanhelvey27@gmail.com>
2020-04-22 16:34:32 -0400

    Triplets Test

--- 07-SortAlgos/00-Findtriplets/findTriplets.test.js
+++ 07-SortAlgos/00-Findtriplets/findTriplets.test.js
@@ -1,1 +1,21 @@
 // Test File
+
+const triplet = require("./findTriplets");
+
+// Two differant ways to write jest Tests!
+
+test("Outputs Triplets", function() {
+  expect(triplet([12, 3, 4, 1, 1, 1], 24)).toBe(false)
+});
+
+
+describe('Triplet', () => {
+  it("is a function", () => {
+    expect(typeof triplet).toEqual("function");
+  });
+
+  it("returns true or false.", () => {
+    let returnedValue = triplet([12, 3, 4, 8, 1, 1], 24);
+    expect(returnedValue).toEqual(true);
+  });
+})
